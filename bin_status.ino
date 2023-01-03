215517475ce4c9272413172ef1a46a474855885c
NALLAPRANAY <pranay6243@gmail.com>
2023-01-03 12:26:13 +0530

    Update bin_status.ino

--- bin_status.ino
+++ bin_status.ino
@@ -13,28 +13,42 @@ int level(){
 void op(){
   
   if(distance==LOW and status_lvl>=8){
+    //Serial.println("open");
+    myservo.attach(servopin);
+    for(pos=0;pos<=90;pos++){
+      myservo.write(pos);
+      delay(5);
+    }
+    delay(6000);
+    for(pos=90;pos>=0;pos--){
+      myservo.write(pos);
+      delay(15);
+    }
+//    myservo.write(90);
+//    delay(5000);
+//    myservo.write(0);
     lcd.clear();
     I2C();
-    while(count<num_of_steps){
-      
-      steps();
-      count++;
-      }
-      delay(6000);
-    count=0;
-  while(count<num_of_steps){
     
-    reverse();
-    count++;
+    //cap();
   }
-  count=0;
-  }
+  
   else if(distance==LOW and status_lvl<8){
+//    myservo.write(0);
+//    myservo.detach();
     lcd.clear();
     I2C();
     tone(buzzer,1500);
     delay(1000);
     noTone(buzzer);
     delay(1000);
+    //Serial.println("close");
+    
+    
+  }
+  else if(distance==HIGH){
+    myservo.write(0);
+    delay(1000);
+    myservo.detach();
   }
 }
